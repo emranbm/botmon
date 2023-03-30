1c81b98f5d6393c00a70ba698f7965908cbaf810
Emran Batmanghelich <emran.bm@gmail.com>
2023-03-31 03:05:19 +0330

    Feat: Add alert sender microservice

--- backend/main/alert_sender/base.py
+++ backend/main/alert_sender/base.py
@@ -1,4 +1,7 @@
+import logging
 from abc import ABC, abstractmethod
+
+from asgiref.sync import sync_to_async
 
 from main.models import Alert
 
@@ -12,3 +15,16 @@ class AlertSender(ABC):
         :return: True if the users are successfully informed. Otherwise False.
         """
         pass
+
+    async def send_appropriate_alerts(self):
+        alerts = Alert.objects.filter(fixed_at=None, sent=False).aiterator()
+        total_count = 0
+        sent_count = 0
+        async for alert in alerts:
+            total_count += 1
+            sent = await self.send_alert(alert)
+            if sent:
+                sent_count += 1
+                alert.sent = True
+                await sync_to_async(alert.save)()
+        logging.info(f"Sent {sent_count} alerts out of {total_count}.")
--- backend/main/alert_sender/test_telegramic_alert_sender.py
+++ backend/main/alert_sender/test_telegramic_alert_sender.py
@@ -13,8 +13,8 @@ class TelegramicAlertSenderTest(TestCase):
     @testing_utils.mock_telegram_bot_engine_async
     async def test_should_send_alert(self, mocked_telegram_app: Application):
         user, bot = await testing_utils.create_user_and_their_bot_async()
-        alert = await Alert.objects.acreate(target_bot=bot)
-        await TelegramicAlertSender().send_alert(alert)
+        await Alert.objects.acreate(target_bot=bot)
+        await TelegramicAlertSender().send_appropriate_alerts()
         mocked_send_message: AsyncMock = mocked_telegram_app.bot.send_message
         mocked_send_message.assert_called_once()
         self.assertEqual(user.telegram_chat_id, mocked_send_message.call_args_list[0].kwargs['chat_id'])
@@ -22,8 +22,8 @@ class TelegramicAlertSenderTest(TestCase):
     @testing_utils.mock_telegram_bot_engine_async
     async def test_alert_should_contain_appropriate_message(self, mocked_telegram_app: Application):
         user, bot = await testing_utils.create_user_and_their_bot_async()
-        alert = await Alert.objects.acreate(target_bot=bot)
-        await TelegramicAlertSender().send_alert(alert)
+        await Alert.objects.acreate(target_bot=bot)
+        await TelegramicAlertSender().send_appropriate_alerts()
         mocked_send_message: AsyncMock = mocked_telegram_app.bot.send_message
         alert_text: str = mocked_send_message.call_args_list[0].kwargs['text']
         self.assertTrue(bot.telegram_username in alert_text)
@@ -33,17 +33,17 @@ class TelegramicAlertSenderTest(TestCase):
     @testing_utils.mock_telegram_bot_engine_async
     async def test_should_not_send_fixed_alert(self, mocked_telegram_app: Application):
         user, bot = await testing_utils.create_user_and_their_bot_async()
-        alert = await Alert.objects.acreate(target_bot=bot, fixed_at=timezone.now())
+        await Alert.objects.acreate(target_bot=bot, fixed_at=timezone.now())
         with self.assertRaises(AssertionError):
-            await TelegramicAlertSender().send_alert(alert)
+            await TelegramicAlertSender().send_appropriate_alerts()
         mocked_send_message: AsyncMock = mocked_telegram_app.bot.send_message
         mocked_send_message.assert_not_called()
 
     @testing_utils.mock_telegram_bot_engine_async
     async def test_should_not_send_already_sent_alert(self, mocked_telegram_app: Application):
         user, bot = await testing_utils.create_user_and_their_bot_async()
-        alert = await Alert.objects.acreate(target_bot=bot, sent=True)
+        await Alert.objects.acreate(target_bot=bot, sent=True)
         with self.assertRaises(AssertionError):
-            await TelegramicAlertSender().send_alert(alert)
+            await TelegramicAlertSender().send_appropriate_alerts()
         mocked_send_message: AsyncMock = mocked_telegram_app.bot.send_message
         mocked_send_message.assert_not_called()
